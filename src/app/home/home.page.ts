126374ccb15968cd53283919384557080a308578
CETECOM_AVARAS <cetecom_avaras@duoc.cl>
2024-08-23 11:16:42 -0400

    23-08-2024

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,25 @@ export class HomePage {
 
   constructor() {}
 
+  usuario = {
+    username : "",
+    password : "",
+  }
+  mensaje = "";
+
+
+  validar() {
+    if (this.usuario.username.length >= 3) {
+      if (this.usuario.password.length != 4){
+        
+      }else{
+        console.log('No contiene el minimo de caracteres');
+        this.mensaje = 'Contraseña no cumple';
+      }
+    
+    }else{
+      console.log('Usuario incorrecto');
+      this.mensaje = 'Usuario no cumple';
+    }
+  }
 }
